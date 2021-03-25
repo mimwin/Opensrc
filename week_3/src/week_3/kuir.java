08f14cc7a86564552cbab8f7079f7d18e042c27b
mimwin <kms105903@naver.com>
2021-03-25 10:41:50 +0900

    3 Assignment : Make inverted file 'index.post' from 'index.xml'

--- week_3/src/week_3/kuir.java
+++ week_3/src/week_3/kuir.java
@@ -8,7 +8,7 @@ import org.xml.sax.SAXException;
 
 public class kuir {
 
-	public static void main(String[] args) throws IOException, ParserConfigurationException, TransformerException, SAXException {
+	public static void main(String[] args) throws IOException, ParserConfigurationException, TransformerException, SAXException, ClassNotFoundException {
 		
 		System.out.println("201911155 김미승");
 		
@@ -17,16 +17,18 @@ public class kuir {
 		 * makeKeyword keyword = new makeKeyword(); keyword.ExtractKeyword(
 		 * "C:\\Users\\kms10\\Desktop\\gitstudy\\week_3\\collections.xml");
 		 */
-		 
-		
+
 		 if(args[0].equals("-c")) { 
 			 makeCollection collect = new makeCollection();
 			 collect.ReadHTMLS(args[1]); 
 		 } 
 		 else if(args[0].equals("-k")) { 
-			 makeKeyword
-			 keyword = new makeKeyword(); 
+			 makeKeyword keyword = new makeKeyword(); 
 			 keyword.ExtractKeyword(args[1]); 
+		 }
+		 else if(args[0].contentEquals("-i")) {
+			makeInvert in = new makeInvert();
+			in.Invert(args[1]);
 		 }
 	}
 
---
+++ week_3/src/week_3/makeInvert.java
@@ -0,0 +1,165 @@
+package week_3;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Vector;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class makeInvert {
+	
+	public void Invert(String path) throws IOException, ParserConfigurationException, SAXException, ClassNotFoundException {
+		FileOutputStream filestream = new FileOutputStream("index.post");
+		ObjectOutputStream objectOutputStream = new ObjectOutputStream(filestream);
+		
+		//HashMap for index.post
+		HashMap<String, String> keyMap = new HashMap();
+		
+		//Parsing index.xml
+		DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+		Document Xml = docBuilder.parse(path);
+		Element root = Xml.getDocumentElement();
+		NodeList nodeList = root.getChildNodes();
+		
+		// Vector and HashMap for checking keywords 
+		Vector<String[]> token = new Vector<String[]>();
+		HashMap<String,Integer> hash = new HashMap<String,Integer>();
+		
+		
+		for(int i=0;i<nodeList.getLength();i++) {
+		
+			Node node = nodeList.item(i);
+			NodeList list = node.getChildNodes();
+			
+			//Parsing Term and Term Frequency
+			String Body = list.item(1).getTextContent();
+			token.add(Body.split("#"));
+			
+			//Term and Inverse Document Frequency --> HashMap
+			for(int j=0;j<token.get(i).length;j++) {
+			
+				String[] a = token.get(i);
+				String []id_weight = a[j].split(":");
+				
+				if(hash.containsKey(id_weight[0])) {
+					
+					int q = hash.get(id_weight[0]);
+					hash.put(id_weight[0], q+1);
+				
+				}
+				else hash.put(id_weight[0], 1);
+			}	
+		}
+		
+		
+		for(int i=0;i<nodeList.getLength();i++) {
+						
+			for(int j=0;j<token.get(i).length;j++) {
+				
+				String[] a = token.get(i);
+				String []id_weight = a[j].split(":");
+				
+				//List for keyMap value
+				List<String> list = Arrays.asList(new String[] {"0","0","1","0","2","0","3","0","4","0"});
+				
+				// keyword
+				String id = id_weight[0];
+				
+				if(keyMap.containsKey(id))continue;
+				
+				//IDF 
+				int cnt = hash.get(id);
+				
+				//TF
+				int weight = Integer.parseInt(id_weight[1]);
+				
+				//가중치 -> List
+				double W = weight * Math.log10(5.0/cnt);
+				list.set(i*2+1,Double.toString(W));
+
+				
+				//If IDF>1, update value
+				if(cnt>1) {
+					
+					for(int k=0;k<5;k++) {
+						
+						if(k==i)continue;
+
+						String[] b= token.get(k);
+						int index=-1;
+						Vector<String> vec = new Vector<String>();
+						
+						// Parsing Term and Term Frequency
+						// Find the location of this.keyword and update value
+						for(int n=0;n<b.length;n++) {
+							String[] str = b[n].split(":");
+							if(id.equals(str[0])) {
+								int w = Integer.parseInt(str[1]);
+								W = w * Math.log10(5.0/cnt);
+								list.set(k*2+1,Double.toString(W));
+							}
+						}
+					}
+				}
+				
+				// List -> HashMap value
+				String value ="[ ";
+				for(int k=0;k<list.size();k+=2) {
+					value+=list.get(k)+" "+list.get(k+1);
+					if(k!=list.size()-2)value+=", ";
+				}
+				value +=" ]";
+				
+				keyMap.put(id, value);
+			}
+
+		}
+		
+		objectOutputStream.writeObject(keyMap);
+		objectOutputStream.close();
+		System.out.println("성공!");
+		
+		//Print index.post
+		printPost();
+	}
+	
+	public void printPost() throws IOException, ClassNotFoundException {
+		FileInputStream filestream = new FileInputStream("C:\\Users\\kms10\\Desktop\\gitstudy\\week_3\\index.post");
+		ObjectInputStream objectInputStream = new ObjectInputStream(filestream);
+		
+		Object object = objectInputStream.readObject();
+		objectInputStream.close();
+		
+		System.out.println("읽어온 객체의 type -> "+object.getClass());
+		
+		HashMap hashmap = (HashMap)object;
+		Iterator it = hashmap.keySet().iterator();
+		
+		while(it.hasNext()) {
+			String key = (String) it.next();
+			String value = (String)hashmap.get(key);
+			System.out.println(key + " -> " + value);
+		}
+		
+	}
+
+}
